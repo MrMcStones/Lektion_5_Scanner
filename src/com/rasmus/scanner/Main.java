8a30addfaf362080a9bc16d59f715c3aa470e5ad
MrMcStones <rasmus.ke.nilsson@gmail.com>
2023-09-19 13:42:20 +0200

    Student scanner implementation

--- src/com/rasmus/scanner/Main.java
+++ src/com/rasmus/scanner/Main.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // Objects
         Scanner sc = new Scanner(System.in);
+        Student benny = new Student();
 
+        // Game
         do {
 
-            System.out.println("input Number!");
+            System.out.println("input Benny's age!");
             int result = sc.nextInt();
 
+            benny.age = result;
+
             System.out.println("You picked: " + result);
+            System.out.println("Benny's age is: " + benny.age);
 
         } while (true);
     }
---
+++ src/com/rasmus/scanner/Student.java
@@ -0,0 +1,8 @@
+package com.rasmus.scanner;
+
+public class Student {
+
+    public String name;
+    public int age;
+    public boolean isTired;
+}
