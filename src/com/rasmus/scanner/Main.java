f808dfcea2baf55b78630c9141f596d169bbaf77
MrMcStones <rasmus.ke.nilsson@gmail.com>
2023-09-19 13:14:29 +0200

    Scanner practise

--- src/com/rasmus/scanner/Main.java
+++ src/com/rasmus/scanner/Main.java
@@ -1,8 +1,42 @@
 package com.rasmus.scanner;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("App is running");
+        boolean isPlaying = true;
+
+        do {
+            int result = scannerNumber();
+
+            if (result > 1000) {
+                isPlaying = false;
+                break;
+            }
+
+            for (int i = 0; i < result; i++) {
+                System.out.println(i);
+                System.out.println("looping...");
+            }
+
+        } while (isPlaying);
+    }
+
+    public static int scannerNumber() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please insert a number");
+
+        return sc.nextInt();
+    }
+
+    public static String scannerText() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please insert a random sentence");
+        String input = sc.nextLine();
+
+        return input;
     }
 }
